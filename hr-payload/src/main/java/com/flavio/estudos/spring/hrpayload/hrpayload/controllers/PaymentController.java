575b47b965f7b3fa2a176c44828bdbf6210f73c0
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-06-12 15:40:58 -0300

    Created payment's controller

---
+++ hr-payload/src/main/java/com/flavio/estudos/spring/hrpayload/hrpayload/controllers/PaymentController.java
@@ -0,0 +1,25 @@
+package com.flavio.estudos.spring.hrpayload.hrpayload.controllers;
+
+import com.flavio.estudos.spring.hrpayload.hrpayload.entities.Payment;
+import com.flavio.estudos.spring.hrpayload.hrpayload.services.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentController {
+
+    @Autowired
+    private PaymentService service;
+
+    @GetMapping(value = "/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable long workerId, @PathVariable int days){
+        Payment payment = service.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+
+}
