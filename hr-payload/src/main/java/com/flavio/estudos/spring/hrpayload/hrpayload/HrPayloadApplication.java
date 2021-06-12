1b183714833d059d56fe726e3de0bab64a3d2d6d
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-06-12 15:08:11 -0300

    Created payload1s microservice

---
+++ hr-payload/src/main/java/com/flavio/estudos/spring/hrpayload/hrpayload/HrPayloadApplication.java
@@ -0,0 +1,13 @@
+package com.flavio.estudos.spring.hrpayload.hrpayload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPayloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPayloadApplication.class, args);
+	}
+
+}
---
+++ hr-payload/src/test/java/com/flavio/estudos/spring/hrpayload/hrpayload/HrPayloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.estudos.spring.hrpayload.hrpayload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
