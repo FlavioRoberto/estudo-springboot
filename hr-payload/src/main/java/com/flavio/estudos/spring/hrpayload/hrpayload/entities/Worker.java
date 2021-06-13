72e737a990095f01d7b4eb1cbbb7cf5b4436928c
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-06-13 13:38:05 -0300

    Created RestTemplate

---
+++ hr-payload/src/main/java/com/flavio/estudos/spring/hrpayload/hrpayload/entities/Worker.java
@@ -0,0 +1,33 @@
+package com.flavio.estudos.spring.hrpayload.hrpayload.entities;
+
+import java.io.Serializable;
+
+public class Worker implements Serializable {
+
+    private Long id;
+    private String name;
+    private Double dailyIncome;
+
+    public Worker() {
+    }
+
+    public Worker(Long id, String name, Double dailyIncome) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.dailyIncome = dailyIncome;
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Double getDailyIncome() {
+        return dailyIncome;
+    }
+}
