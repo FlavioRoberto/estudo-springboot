691661656bc87463e305be05a447c35629e8aad0
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-06-13 15:56:38 -0300

    Add FeignClient

--- hr-payload/src/main/java/com/flavio/estudos/spring/hrpayload/hrpayload/services/PaymentService.java
+++ hr-payload/src/main/java/com/flavio/estudos/spring/hrpayload/hrpayload/services/PaymentService.java
@@ -2,28 +2,18 @@ package com.flavio.estudos.spring.hrpayload.hrpayload.services;
 
 import com.flavio.estudos.spring.hrpayload.hrpayload.entities.Payment;
 import com.flavio.estudos.spring.hrpayload.hrpayload.entities.Worker;
+import com.flavio.estudos.spring.hrpayload.hrpayload.feignclients.WorkerFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
     @Autowired
-    private RestTemplate restTemplate;
+    private WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uriVariables = new HashMap<>();
-        uriVariables.put("id", ""+workerId);
-
-        Worker worker = restTemplate.getForObject(workerHost+"/workers/{id}", Worker.class, uriVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
