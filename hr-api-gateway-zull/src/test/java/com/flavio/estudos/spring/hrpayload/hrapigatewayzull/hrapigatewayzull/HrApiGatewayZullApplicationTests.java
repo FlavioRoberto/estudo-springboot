cad855475041c66155b937b840ea35997130677d
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-06-14 14:15:29 -0300

    Project hr-api-gateway created

---
+++ hr-api-gateway-zull/src/test/java/com/flavio/estudos/spring/hrpayload/hrapigatewayzull/hrapigatewayzull/HrApiGatewayZullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.estudos.spring.hrpayload.hrapigatewayzull.hrapigatewayzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
