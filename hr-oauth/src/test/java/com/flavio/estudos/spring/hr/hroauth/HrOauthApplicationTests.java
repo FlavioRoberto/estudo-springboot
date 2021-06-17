f3ad2f07fb14ab1b06cb8f074a640a10a0aaee90
Flávio Roberto Teixeira <flaviolive3@hotmail.com>
2021-06-17 20:24:38 -0300

    Creating hr-oauth project

---
+++ hr-oauth/src/test/java/com/flavio/estudos/spring/hr/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flavio.estudos.spring.hr.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
